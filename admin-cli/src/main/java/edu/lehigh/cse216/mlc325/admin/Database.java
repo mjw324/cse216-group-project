d193abdadba5f5552141092ec74de8b8c24d59cd
mlc325 <mlc325@lehigh.edu>
2022-09-19 12:46:07 -0400

    copied over phase0 code as a starting point

---
+++ admin-cli/src/main/java/edu/lehigh/cse216/mlc325/admin/Database.java
@@ -0,0 +1,343 @@
+package edu.lehigh.cse216.mlc325.admin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.net.URISyntaxException;
+import java.net.URI;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Database {
+    /**
+     * The connection to the database.  When there is no connection, it should
+     * be null.  Otherwise, there is a valid open connection
+     */
+    private Connection mConnection;
+
+    /**
+     * A prepared statement for getting all data in the database
+     */
+    private PreparedStatement mSelectAll;
+
+    /**
+     * A prepared statement for getting one row from the database
+     */
+    private PreparedStatement mSelectOne;
+
+    /**
+     * A prepared statement for deleting a row from the database
+     */
+    private PreparedStatement mDeleteOne;
+
+    /**
+     * A prepared statement for inserting into the database
+     */
+    private PreparedStatement mInsertOne;
+
+    /**
+     * A prepared statement for updating a single row in the database
+     */
+    private PreparedStatement mUpdateOne;
+
+    /**
+     * A prepared statement for creating the table in our database
+     */
+    private PreparedStatement mCreateTable;
+
+    /**
+     * A prepared statement for dropping the table in our database
+     */
+    private PreparedStatement mDropTable;
+
+    /**
+ * DataRow holds a row of information.  A row of information consists of
+ * an identifier, strings for a "title" and "content", and a creation date.
+ * 
+ * Because we will ultimately be converting instances of this object into JSON
+ * directly, we need to make the fields public.  That being the case, we will
+ * not bother with having getters and setters... instead, we will allow code to
+ * interact with the fields directly.
+ */
+public class DataRow {
+    /**
+     * The unique identifier associated with this element.  It's final, because
+     * we never want to change it.
+     */
+    public final int mId;
+
+    /**
+     * The title for this row of data
+     */
+    public String mTitle;
+
+    /**
+     * The content for this row of data
+     */
+    public String mContent;
+
+    /**
+     * The creation date for this row of data.  Once it is set, it cannot be 
+     * changed
+     */
+    public final Date mCreated;
+
+    /**
+     * Create a new DataRow with the provided id and title/content, and a 
+     * creation date based on the system clock at the time the constructor was
+     * called
+     * 
+     * @param id The id to associate with this row.  Assumed to be unique 
+     *           throughout the whole program.
+     * 
+     * @param title The title string for this row of data
+     * 
+     * @param content The content string for this row of data
+     */
+    DataRow(int id, String title, String content) {
+        mId = id;
+        mTitle = title;
+        mContent = content;
+        mCreated = new Date();
+    }
+
+    /**
+     * Copy constructor to create one datarow from another
+     */
+    DataRow(DataRow data) {
+        mId = data.mId;
+        // NB: Strings and Dates are immutable, so copy-by-reference is safe
+        mTitle = data.mTitle;
+        mContent = data.mContent;
+        mCreated = data.mCreated;
+    }
+}
+
+    /**
+     * The Database constructor is private: we only create Database objects 
+     * through the getDatabase() method.
+     */
+    private Database() {
+    }
+
+    /**
+     * Get a fully-configured connection to the database
+     * 
+     * @param ip   The IP address of the database server
+     * @param port The port on the database server to which connection requests
+     *             should be sent
+     * @param user The user ID to use when connecting
+     * @param pass The password to use when connecting
+     * 
+     * @return A Database object, or null if we cannot connect properly
+     */
+    static Database getDatabase(String db_url) {
+        // Create an un-configured Database object
+        Database db = new Database();
+
+        // Give the Database object a connection, fail if we cannot get one
+        try {
+            Class.forName("org.postgresql.Driver");
+            URI dbUri = new URI(db_url);
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+            Connection conn = DriverManager.getConnection(dbUrl, username, password);
+            if (conn == null) {
+                System.err.println("Error: DriverManager.getConnection() returned a null object");
+                return null;
+            }
+            db.mConnection = conn;
+        } catch (SQLException e) {
+            System.err.println("Error: DriverManager.getConnection() threw a SQLException");
+            e.printStackTrace();
+            return null;
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("Unable to find postgresql driver");
+            return null;
+        } catch (URISyntaxException s) {
+            System.out.println("URI Syntax Error");
+            return null;
+        }
+
+        // Attempt to create all of our prepared statements.  If any of these 
+        // fail, the whole getDatabase() call should fail
+        try {
+            // NB: we can easily get ourselves in trouble here by typing the
+            //     SQL incorrectly.  We really should have things like "tblData"
+            //     as constants, and then build the strings for the statements
+            //     from those constants.
+
+            // Note: no "IF NOT EXISTS" or "IF EXISTS" checks on table 
+            // creation/deletion, so multiple executions will cause an exception
+            db.mCreateTable = db.mConnection.prepareStatement(
+                    "CREATE TABLE tblData (id SERIAL PRIMARY KEY, title VARCHAR(50) "
+                    + "NOT NULL, content VARCHAR(500) NOT NULL)");
+            db.mDropTable = db.mConnection.prepareStatement("DROP TABLE tblData");
+
+            // Standard CRUD operations
+            db.mDeleteOne = db.mConnection.prepareStatement("DELETE FROM tblData WHERE id = ?");
+            db.mInsertOne = db.mConnection.prepareStatement("INSERT INTO tblData VALUES (default, ?, ?)");
+            db.mSelectAll = db.mConnection.prepareStatement("SELECT id, title FROM tblData");
+            db.mSelectOne = db.mConnection.prepareStatement("SELECT * from tblData WHERE id=?");
+            db.mUpdateOne = db.mConnection.prepareStatement("UPDATE tblData SET content = ? WHERE id = ?");
+        } catch (SQLException e) {
+            System.err.println("Error creating prepared statement");
+            e.printStackTrace();
+            db.disconnect();
+            return null;
+        }
+        return db;
+    }
+
+    /**
+     * Close the current connection to the database, if one exists.
+     * 
+     * NB: The connection will always be null after this call, even if an 
+     *     error occurred during the closing operation.
+     * 
+     * @return True if the connection was cleanly closed, false otherwise
+     */
+    boolean disconnect() {
+        if (mConnection == null) {
+            System.err.println("Unable to close connection: Connection was null");
+            return false;
+        }
+        try {
+            mConnection.close();
+        } catch (SQLException e) {
+            System.err.println("Error: Connection.close() threw a SQLException");
+            e.printStackTrace();
+            mConnection = null;
+            return false;
+        }
+        mConnection = null;
+        return true;
+    }
+
+    /**
+     * Insert a row into the database
+     * 
+     * @param title The title for this new row
+     * @param content The content for this new row
+     * 
+     * @return The number of rows that were inserted
+     */
+    int insertRow(String title, String content) {
+        int count = 0;
+        try {
+            mInsertOne.setString(1, title);
+            mInsertOne.setString(2, content);
+            count += mInsertOne.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+
+    /**
+     * Query the database for a list of all titles and their IDs
+     * 
+     * @return All rows, as an ArrayList
+     */
+    ArrayList<DataRow> selectAll() {
+        ArrayList<DataRow> res = new ArrayList<DataRow>();
+        try {
+            ResultSet rs = mSelectAll.executeQuery();
+            while (rs.next()) {
+                res.add(new DataRow(rs.getInt("id"), rs.getString("title"), null));
+            }
+            rs.close();
+            return res;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Get all data for a specific row, by ID
+     * 
+     * @param id The id of the row being requested
+     * 
+     * @return The data for the requested row, or null if the ID was invalid
+     */
+    DataRow selectOne(int id) {
+        DataRow res = null;
+        try {
+            mSelectOne.setInt(1, id);
+            ResultSet rs = mSelectOne.executeQuery();
+            if (rs.next()) {
+                res = new DataRow(rs.getInt("id"), rs.getString("title"), rs.getString("content"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    /**
+     * Delete a row by ID
+     * 
+     * @param id The id of the row to delete
+     * 
+     * @return The number of rows that were deleted.  -1 indicates an error.
+     */
+    int deleteRow(int id) {
+        int res = -1;
+        try {
+            mDeleteOne.setInt(1, id);
+            res = mDeleteOne.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    /**
+     * Update the content for a row in the database
+     * 
+     * @param id The id of the row to update
+     * @param content The new content
+     * 
+     * @return The number of rows that were updated.  -1 indicates an error.
+     */
+    int updateOne(int id, String content) {
+        int res = -1;
+        try {
+            mUpdateOne.setString(1, content);
+            mUpdateOne.setInt(2, id);
+            res = mUpdateOne.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    /**
+     * Create tblData.  If it already exists, this will print an error
+     */
+    void createTable() {
+        try {
+            mCreateTable.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Remove tblData from the database.  If it does not exist, this will print
+     * an error.
+     */
+    void dropTable() {
+        try {
+            mDropTable.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
