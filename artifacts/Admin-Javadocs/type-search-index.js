a29b5e3a00d3ac27d507d00ed18a55a092dae041
aas620 <aas620@lehigh.edu>
2022-10-08 20:24:56 -0400

    admin code documentation

---
+++ artifacts/Admin-Javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.lehigh.cse216.mlc325.admin","l":"App"},{"p":"edu.lehigh.cse216.mlc325.admin","l":"Database"},{"p":"edu.lehigh.cse216.mlc325.admin","l":"Database.DataRow"}];updateSearchResults();
